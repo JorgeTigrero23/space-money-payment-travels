ff04aecdd19dbc6da5e5dc2330cf8889e849ed6a
JorgeTigrero23 <JorgeTigrero23>
2021-10-10 00:00:41 +0000

    Initial commit

---
+++ src/main/java/com/space/money/payment/travel/Application.java
@@ -0,0 +1,10 @@
+package com.space.money.payment.travel;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
